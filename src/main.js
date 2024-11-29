3fdb886bc604047869af59e26c5e20d3571afae4
julio <juliopa54@gmail.com>
2024-11-29 20:43:09 +0100

    start on flexbox grid

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+console.log('Hallo');
+const gridContainer = document.querySelector(".grid-container")
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+let gridSize = 16;
 
-setupCounter(document.querySelector('#counter'))
+for (let i = 0; i < gridSize * gridSize; i++) {
+    let gridSquare = document.createElement("div")
+    gridSquare.innerText = "Box"
+    gridSquare.classList.add("item")
+    gridContainer.appendChild(gridSquare)
+    console.log("beep");
+    
+
+}
