d53f29a09bf9162d0e2cb496f850513bd366f5ef
julio <juliopa54@gmail.com>
2024-12-02 14:15:32 +0100

    create flexbox grid

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let gridSize = 16;
 
 for (let i = 0; i < gridSize * gridSize; i++) {
     let gridSquare = document.createElement("div")
-    gridSquare.innerText = "Box"
+    gridSquare.innerText = i
     gridSquare.classList.add("item")
     gridContainer.appendChild(gridSquare)
     console.log("beep");
